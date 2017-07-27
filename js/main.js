fc2f99ac5f3394a0667acef3223f29a64d579b9c
Marc Rabinowitz <marcrabinowitz@Marcs-MacBook-Pro.local>
2017-07-27 16:51:46 -0700

    Second Commit

--- js/main.js
+++ js/main.js
@@ -14,10 +14,19 @@ var userLogin = [
   },
 ];
 
+//Allows the user to press the enter key to login
 var btn = document.getElementById("password");
     btn.addEventListener("keypress", function enterKey(e) {
     if (e.keyCode == 13) {
         logMeIn();
+    }
+},false);
+
+//Allows the user to press the enter key to register as a new user
+var btn = document.getElementById("registerPassword");
+    btn.addEventListener("keypress", function enterKey(e) {
+    if (e.keyCode == 13) {
+        registerMe();
     }
 },false);
 
@@ -49,4 +58,30 @@ function logMeIn() {
   console.log("Incorrect Password")
   results.className = "failure"
   results.textContent = "Incorrect Username, " + username + " Please try again!";
+};
+
+// HTML line #45 is the onclick that registers new users
+function registerMe() {
+  //stores variable for new users names
+  var registerUser = document.getElementById('registerUser').value
+  //stores variable for new users passwords
+  var registerPassword = document.getElementById('registerPassword').value
+  //stores variable so that registerUser and registerPassword can be used as an object for userLogin
+  var newUser = {
+    user: registerUser,
+    pass: registerPassword
+  };
+//Loop for searching if users names already exsist. Also, wont let users create a password with less than 8 characters
+  for (var i = 0; i < userLogin.length; i++) {
+        if (registerUser == userLogin[i].user) {
+          alert ("That username is already in use, please choose another login!")
+          return;
+        } else if (registerPassword.length < 8) {
+          alert ("Password must be longer than 8 characters")
+          return;
+        };
+  };
+//Adds new user to the object userLogin
+  userLogin.push(newUser)
+  console.log(userLogin);
 };
